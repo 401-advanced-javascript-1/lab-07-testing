2b1fcecfd8bdb133baafffce46308859acc1bc07
Cory Henderson <chenderson19899@gmail.com>
2019-03-26 18:03:52 -0700

    starts testing

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# lab-07-testing
+# JS401 - Lab-07-Testing
+This lab demonstrates how to test a simple server and associated middleware.
+
+## Author: Cory Henderson
+
+## Links and Resources
+- [Github Repo](https://github.com/401-advanced-javascript-1/lab-07-testing)
+- [Travis](https://www.travis-ci.com/401-advanced-javascript-1/lab-07-testing)
+
+## Documentation
+
+# Modules
+- server.js
+- server.test.js
+
+# Setup
+
+## Running the app
+- npm test will run the jest test package for the application.
+
+## Tests
+- Tests check that proper status codes are returned for various paths.
+- Tests utilize mock unit testing to check middleware for req, res, and next. A spy is set on the next parameter to ensure proper callback.
+
+## UML
--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -2,10 +2,12 @@
 
 const supertest = require('supertest');
 
-// What's up with the {}????
+// What's up with the {}???? deconstructoring const server = export server
 const {server} = require('../server.js');
-
 const mockClient = supertest(server);
+let next = () => {};
+let req = () => {};
+let res = () => {status: '500', send: 'Error'};
 
 describe('The Server', () => {
   it('responds with a 200 on a good route', () => {
@@ -17,9 +19,23 @@ describe('The Server', () => {
   });
 
   it('responds with a 500 on an error', () => {
+    return mockClient.get('/e')
+      .then( result => {
+        expect(result.status).toEqual(500);
+      });
   });
 
   it('responds with a 404 on an unknown route', () => {
+    return mockClient.get('/x')
+      .then( result => {
+        expect(result.status).toEqual(404);
+      });
   });
-  
+});
+
+// describe('The middleware', () => {
+//   it('Correctly handles a request', () => {
+//     return mockClient.get('/e', )
+//     expect()
+//   });
 });
